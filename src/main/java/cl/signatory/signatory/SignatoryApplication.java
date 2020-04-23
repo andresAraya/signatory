5b62409d10c28ac5b204d3ec74bf781c40e2eecc
Arnaldo Araya <aaraya@digital.gob.cl>
2020-04-22 23:00:38 -0400

    agregando proyecto a repositorio

---
+++ src/main/java/cl/signatory/signatory/SignatoryApplication.java
@@ -0,0 +1,13 @@
+package cl.signatory.signatory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignatoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignatoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/signatory/signatory/SignatoryApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.signatory.signatory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignatoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
